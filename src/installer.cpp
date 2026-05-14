6698b898b2cfa14bf997c0a931587e7f578d84fb
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2026-05-14 08:41:30 -0700

    DEBUG: log harvestCookies + restoreReply bodies (revert after triage)

--- src/installer.cpp
+++ src/installer.cpp
@@ -874,15 +874,21 @@ void InstallNet::determineDeviceFamily()
 // that errored mid-stream (BB10 closes TLS without close_notify).
 void InstallNet::harvestCookies(QNetworkReply* r)
 {
-    if (!r || !cookieJar)
+    if (!r || !cookieJar) {
+        qDebug() << "harvestCookies: r=" << r << " cookieJar=" << cookieJar;
         return;
+    }
     QUrl url = r->url();
+    int harvested = 0;
     foreach (const QNetworkReply::RawHeaderPair& h, r->rawHeaderPairs()) {
         if (h.first.toLower() != "set-cookie")
             continue;
         QList<QNetworkCookie> cookies = QNetworkCookie::parseCookies(h.second);
         cookieJar->setCookiesFromUrl(cookies, url);
+        harvested += cookies.size();
     }
+    qDebug() << "harvestCookies: url=" << url.toString() << " harvested=" << harvested
+             << " jar now has=" << cookieJar->cookiesForUrl(url).size() << " for this URL";
 }
 
 void InstallNet::restoreReply()
@@ -892,9 +898,9 @@ void InstallNet::restoreReply()
 
     harvestCookies(reply);
     QByteArray data = reply->readAll();
-#if DEBUG_LOG
-    for (int s = 0; s < data.size(); s+=3500) qDebug() << "Message:\n" << QString(data).simplified().mid(s, 3500);
-#endif
+    qDebug() << "restoreReply: url=" << reply->url().toString()
+             << " bytes=" << data.size()
+             << " bodyHead=" << QString(data).simplified().left(300);
     if (data.size() == 0) {
         if (_restoring) {
             QMessageBox::information(nullptr, "Restore Error", "There was an error loading the backup file.\nThe device encountered an unrecoverable bug.\nIt is not designed to restore this backup.");
